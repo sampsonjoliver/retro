3dd77be73a24e0ef690f7bbe9601405d3d344baf
sampsonoliver <sampsonjoliver@outlook.com>
2018-06-25 00:20:11 -0700

    Add material ui and other deps

---
+++ apps/app/pages/_document.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import Document, { Head, Main, NextScript } from 'next/document';
+import JssProvider from 'react-jss/lib/JssProvider';
+import flush from 'styled-jsx/server';
+import getPageContext from '../getPageContext';
+
+class MyDocument extends Document {
+  render() {
+    const { pageContext } = this.props;
+
+    return (
+      <html lang="en" dir="ltr">
+        <Head>
+          <title>Retro</title>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content={
+              'user-scalable=0, initial-scale=1, ' +
+              'minimum-scale=1, width=device-width, height=device-height'
+            }
+          />
+          <meta
+            name="theme-color"
+            content={pageContext.theme.palette.primary.main}
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+          />
+          <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = ctx => {
+  const pageContext = getPageContext();
+  const page = ctx.renderPage(Component => props => (
+    <JssProvider
+      registry={pageContext.sheetsRegistry}
+      generateClassName={pageContext.generateClassName}
+    >
+      <Component pageContext={pageContext} {...props} />
+    </JssProvider>
+  ));
+
+  return {
+    ...page,
+    pageContext,
+    styles: (
+      <React.Fragment>
+        <style
+          id="jss-server-side"
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: pageContext.sheetsRegistry.toString()
+          }}
+        />
+        {flush() || null}
+      </React.Fragment>
+    )
+  };
+};
+
+export default MyDocument;
