f7a1f04680be88602ad41621d653e2ba3c867e73
sampsonoliver <sampsonjoliver@outlook.com>
2018-09-09 23:08:09 +1000

    Add config strategy for prod

---
+++ apps/configure.sh
@@ -0,0 +1,8 @@
+yarn firebase functions:config:set \
+    fb.apikey=${REACT_APP_FIREBASE_APIKEY} \
+    fb.authdomain=${REACT_APP_FIREBASE_AUTH_DOMAIN} \
+    fb.databaseurl=${REACT_APP_FIREBASE_DATABASE_URL} \
+    fb.projectid=${REACT_APP_FIREBASE_PROJECT_ID} \
+    fb.storagebucket=${REACT_APP_FIREBASE_STORAGE_BUCKET} \
+    fb.messagingsenderid=${REACT_APP_FIREBASE_MESSAGING_SENDER_ID}
+
