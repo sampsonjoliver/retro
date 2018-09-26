421baadfeef4e8570c13eb71bb615427a0da1803
sampsonoliver <sampsonjoliver@outlook.com>
2018-09-26 14:58:30 +1000

    Mock out things in App test

--- apps/src/__tests__/App.test.tsx
+++ apps/src/__tests__/App.test.tsx
@@ -2,6 +2,13 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from '../App';
 
+jest.mock('../services/firebase');
+jest.mock('../services/firestore');
+jest.mock('../services/auth');
+jest.mock('../pages/login');
+jest.mock('../pages/index');
+jest.mock('../pages/sprint');
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
